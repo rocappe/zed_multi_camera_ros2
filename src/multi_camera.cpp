c0ac56f419c5c7f142b97128a3105763dc036aff
rocappe <cappellaro.roberto@gmail.com>
2021-11-02 17:03:15 +0000

    Start implementing odom publisher

--- src/multi_camera.cpp
+++ src/multi_camera.cpp
@@ -9,20 +9,22 @@
 #include <sensor_msgs/msg/image.hpp>
 #include <sensor_msgs/distortion_models.hpp>
 #include <sensor_msgs/msg/camera_info.hpp>
+#include <nav_msgs/msg/odometry>
 
 
 class ZedPublisher : public rclcpp::Node
 {
   public:
-	  ZedPublisher();
-	  ~ZedPublisher();
+    ZedPublisher();
+    ~ZedPublisher();
 
   private:
     int main_loop();
     void get_and_publish(sl::Camera& zed, bool& run);
-	  void fillCamInfo(sl::Camera& zed, sl::Resolution& resolution, std::shared_ptr<sensor_msgs::msg::CameraInfo> leftCamInfoMsg, 
-							std::shared_ptr<sensor_msgs::msg::CameraInfo> rightCamInfoMsg, std::string leftFrameId,
-							std::string rightFrameId, bool rawParam = false);
+    void fillCamInfo(sl::Camera& zed, sl::Resolution& resolution, std::shared_ptr<sensor_msgs::msg::CameraInfo> leftCamInfoMsg, 
+              std::shared_ptr<sensor_msgs::msg::CameraInfo> rightCamInfoMsg, std::string leftFrameId,
+              std::string rightFrameId, bool rawParam = false);
+    void publish_odometry(sl::Camera& zed, bool& run);
 
     bool run_;
     int res_w_;
@@ -37,6 +39,7 @@ class ZedPublisher : public rclcpp::Node
     size_t nb_detected_zed_;
     std::vector<std::shared_ptr<sl::Camera>> zeds_;
     std::vector<std::thread> thread_pool_;
+    std::mutex zed_mutex_;
 };
 
 
@@ -57,7 +60,8 @@ ZedPublisher::ZedPublisher() : Node{"zed_rgbd_publisher"}, run_{true}, nb_detect
   auto rgb_publisher_r_ = this->create_publisher<sensor_msgs::msg::Image>("zed2_r/rgb", qos_profile);
   auto depth_publisher_l_ = this->create_publisher<sensor_msgs::msg::Image>("zed2_l/depth", qos_profile);
   auto depth_publisher_r_ = this->create_publisher<sensor_msgs::msg::Image>("zed2_r/depth", qos_profile);
-  
+  auto odom_publisher = create_publisher<nav_msgs::msg::Odometry>("zed2_l/odom", qos_profile);
+
   image_publishers_ = {{sn_l_, {rgb_publisher_l_, depth_publisher_l_}}, {sn_r_, {rgb_publisher_r_, depth_publisher_r_}}};
   info_publishers_ = {{sn_l_, rgb_publisher_l_info_}, {sn_r_, rgb_publisher_r_info_}};
   camera_frames_ = {{sn_l_, "zed2_l_left_camera_optical_frame"},  {sn_r_, "zed2_r_left_camera_optical_frame"}};
@@ -119,13 +123,17 @@ int ZedPublisher::main_loop() {
       if (zeds_.at(z)->isOpened()) {
           // camera acquisition thread
         std::cout << "Camera thread " << z+1 << " spawned" << std::endl;
-          thread_pool_.emplace_back(&ZedPublisher::get_and_publish, this, std::ref(*zeds_.at(z)), std::ref(run_));
+        thread_pool_.emplace_back(&ZedPublisher::get_and_publish, this, std::ref(*zeds_.at(z)), std::ref(run_));
+        if (zeds_.at(z)->getCameraInformation().serial_number == sn_l_){
+          thread_pool_.emplace_back(&ZedPublisher::publish_odometry, this, std::ref(*zeds_.at(z)), std::ref(run_));
+        }
       }
 	}
 };
 
 
 void ZedPublisher::get_and_publish(sl::Camera& zed, bool& run) {
+  const std::lock_guard<std::mutex> lock(zed_mutex_);
   sl::Mat mat_depth;
   sl::Mat mat_image;
   auto left_cam_info_msg = std::make_shared<sensor_msgs::msg::CameraInfo>();
@@ -151,6 +159,284 @@ void ZedPublisher::get_and_publish(sl::Camera& zed, bool& run) {
   zed.close();
 };
 
+void publish_odometry(sl::Camera& zed, bool& run){
+  const std::lock_guard<std::mutex> lock(zed_mutex_);
+}
+
+void ZedCamera::processPose()
+{
+  //adapted from https://github.com/stereolabs/zed-ros2-wrapper/blob/de1dfc4cde9ada3173074a05e14631d4d56e442c/zed_components/src/zed_camera/src/zed_camera_component.cpp
+  
+  if (!mSensor2BaseTransfValid)
+  {
+    getSens2BaseTransform();
+  }
+
+  if (!mSensor2CameraTransfValid)
+  {
+    getSens2CameraTransform();
+  }
+
+  if (!mCamera2BaseTransfValid)
+  {
+    getCamera2BaseTransform();
+  }
+
+  size_t odomSub = 0;
+  try
+  {
+    odomSub = count_subscribers(mOdomTopic);  // mPubOdom subscribers
+  }
+  catch (...)
+  {
+    rcutils_reset_error();
+    RCLCPP_DEBUG(get_logger(), "processPose: Exception while counting subscribers");
+    return;
+  }
+
+  static sl::POSITIONAL_TRACKING_STATE oldStatus;
+  mPosTrackingStatus = mZed.getPosition(mLastZedPose, sl::REFERENCE_FRAME::WORLD);
+
+  sl::Translation translation = mLastZedPose.getTranslation();
+  sl::Orientation quat = mLastZedPose.getOrientation();
+
+  if (quat.sum() == 0)
+  {
+    return;
+  }
+
+#if 0  // Enable for TF checking
+    double roll, pitch, yaw;
+    tf2::Matrix3x3(tf2::Quaternion(quat.ox, quat.oy, quat.oz, quat.ow)).getRPY(roll, pitch, yaw);
+
+    RCLCPP_DEBUG(get_logger(), "Sensor POSE [%s -> %s] - {%.2f,%.2f,%.2f} {%.2f,%.2f,%.2f}",
+                 mLeftCamFrameId.c_str(), mMapFrameId.c_str(),
+                 translation.x, translation.y, translation.z,
+                 roll * RAD2DEG, pitch * RAD2DEG, yaw * RAD2DEG);
+
+    RCLCPP_DEBUG(get_logger(), "MAP -> Tracking Status: %s", sl::toString(mTrackingStatus).c_str());
+#endif
+
+  if (mPosTrackingStatus == sl::POSITIONAL_TRACKING_STATE::OK ||
+      mPosTrackingStatus == sl::POSITIONAL_TRACKING_STATE::SEARCHING)
+  {
+    tf2::Transform map_to_sens_transf;
+    map_to_sens_transf.setOrigin(tf2::Vector3(translation(0), translation(1), translation(2)));
+    map_to_sens_transf.setRotation(tf2::Quaternion(quat(0), quat(1), quat(2), quat(3)));
+
+    mMap2BaseTransf = map_to_sens_transf * mSensor2BaseTransf;  // Base position in map frame
+
+    if (mTwoDMode)
+    {
+      tf2::Vector3 tr_2d = mMap2BaseTransf.getOrigin();
+      tr_2d.setZ(mFixedZValue);
+      mMap2BaseTransf.setOrigin(tr_2d);
+
+      double roll, pitch, yaw;
+      tf2::Matrix3x3(mMap2BaseTransf.getRotation()).getRPY(roll, pitch, yaw);
+
+      tf2::Quaternion quat_2d;
+      quat_2d.setRPY(0.0, 0.0, yaw);
+
+      mMap2BaseTransf.setRotation(quat_2d);
+    }
+
+#if 0  // Enable for TF checking
+        double roll, pitch, yaw;
+        tf2::Matrix3x3(mMap2BaseTransf.getRotation()).getRPY(roll, pitch, yaw);
+
+        RCLCPP_DEBUG(get_logger(), "*** Base POSE [%s -> %s] - {%.3f,%.3f,%.3f} {%.3f,%.3f,%.3f}",
+                     mMapFrameId.c_str(), mBaseFrameId.c_str(),
+                     mMap2BaseTransf.getOrigin().x(), mMap2BaseTransf.getOrigin().y(), mMap2BaseTransf.getOrigin().z(),
+                     roll * RAD2DEG, pitch * RAD2DEG, yaw * RAD2DEG);
+#endif
+
+    bool initOdom = false;
+
+    if (!(mFloorAlignment))
+    {
+      initOdom = mInitOdomWithPose;
+    }
+    else
+    {
+      initOdom = mInitOdomWithPose & (mPosTrackingStatus == sl::POSITIONAL_TRACKING_STATE::OK);
+    }
+
+    if (initOdom || mResetOdom)
+    {
+      RCLCPP_INFO(get_logger(), "Odometry aligned to last tracking pose");
+
+      // Propagate Odom transform in time
+      mOdom2BaseTransf = mMap2BaseTransf;
+      mMap2BaseTransf.setIdentity();
+
+      if (odomSub > 0)
+      {
+        // Publish odometry message
+        publishOdom(mOdom2BaseTransf, mLastZedPose, mFrameTimestamp);
+      }
+
+      mInitOdomWithPose = false;
+      mResetOdom = false;
+    }
+    else
+    {
+      // Transformation from map to odometry frame
+      mMap2OdomTransf = mMap2BaseTransf * mOdom2BaseTransf.inverse();
+
+#if 0  // Enable for TF checking
+            double roll, pitch, yaw;
+            tf2::Matrix3x3(mMap2OdomTransf.getRotation()).getRPY(roll, pitch, yaw);
+
+            RCLCPP_DEBUG(get_logger(), "+++ Diff [%s -> %s] - {%.3f,%.3f,%.3f} {%.3f,%.3f,%.3f}",
+                         mMapFrameId.c_str(), mOdomFrameId.c_str(),
+                         mMap2OdomTransf.getOrigin().x(), mMap2OdomTransf.getOrigin().y(), mMap2OdomTransf.getOrigin().z(),
+                         roll * RAD2DEG, pitch * RAD2DEG, yaw * RAD2DEG);
+#endif
+    }
+
+    // Publish Pose message
+    publishPose();
+    mPosTrackingReady = true;
+  }
+
+  oldStatus = mPosTrackingStatus;
+}
+
+void ZedCamera::processOdometry()
+{
+  //adapted from https://github.com/stereolabs/zed-ros2-wrapper/blob/de1dfc4cde9ada3173074a05e14631d4d56e442c/zed_components/src/zed_camera/src/zed_camera_component.cpp
+  
+  rclcpp::Clock steady_clock(RCL_STEADY_TIME);
+
+  if (!mSensor2BaseTransfValid)
+  {
+    getSens2BaseTransform();
+  }
+
+  if (!mSensor2CameraTransfValid)
+  {
+    getSens2CameraTransform();
+  }
+
+  if (!mCamera2BaseTransfValid)
+  {
+    getCamera2BaseTransform();
+  }
+
+  if (!mInitOdomWithPose)
+  {
+    sl::Pose deltaOdom;
+
+    mPosTrackingStatus = mZed.getPosition(deltaOdom, sl::REFERENCE_FRAME::CAMERA);
+
+    sl::Translation translation = deltaOdom.getTranslation();
+    sl::Orientation quat = deltaOdom.getOrientation();
+
+#if 0
+        RCLCPP_DEBUG(get_logger(), "delta ODOM [%s] - %.2f,%.2f,%.2f %.2f,%.2f,%.2f,%.2f",
+                     sl::toString(mPosTrackingStatus).c_str(),
+                     translation(0), translation(1), translation(2),
+                     quat(0), quat(1), quat(2), quat(3));
+#endif
+
+    if (mPosTrackingStatus == sl::POSITIONAL_TRACKING_STATE::OK ||
+        mPosTrackingStatus == sl::POSITIONAL_TRACKING_STATE::SEARCHING ||
+        mPosTrackingStatus == sl::POSITIONAL_TRACKING_STATE::FPS_TOO_LOW)
+    {
+      // Transform ZED delta odom pose in TF2 Transformation
+      tf2::Transform deltaOdomTf;
+      deltaOdomTf.setOrigin(tf2::Vector3(translation(0), translation(1), translation(2)));
+      // w at the end in the constructor
+      deltaOdomTf.setRotation(tf2::Quaternion(quat(0), quat(1), quat(2), quat(3)));
+
+      // delta odom from sensor to base frame
+      tf2::Transform deltaOdomTf_base = mSensor2BaseTransf.inverse() * deltaOdomTf * mSensor2BaseTransf;
+
+      // Propagate Odom transform in time
+      mOdom2BaseTransf = mOdom2BaseTransf * deltaOdomTf_base;
+
+      if (mTwoDMode)
+      {
+        tf2::Vector3 tr_2d = mOdom2BaseTransf.getOrigin();
+        tr_2d.setZ(mFixedZValue);
+        mOdom2BaseTransf.setOrigin(tr_2d);
+
+        double roll, pitch, yaw;
+        tf2::Matrix3x3(mOdom2BaseTransf.getRotation()).getRPY(roll, pitch, yaw);
+
+        tf2::Quaternion quat_2d;
+        quat_2d.setRPY(0.0, 0.0, yaw);
+
+        mOdom2BaseTransf.setRotation(quat_2d);
+      }
+
+#if 0
+            double roll, pitch, yaw;
+            tf2::Matrix3x3(mOdom2BaseTransf.getRotation()).getRPY(roll, pitch, yaw);
+
+            RCLCPP_DEBUG(get_logger(), "+++ Odometry [%s -> %s] - {%.3f,%.3f,%.3f} {%.3f,%.3f,%.3f}",
+                         mOdomFrameId.c_str(), mBaseFrameId.c_str(),
+                         mOdom2BaseTransf.getOrigin().x(),
+                         mOdom2BaseTransf.getOrigin().y(),
+                         mOdom2BaseTransf.getOrigin().z(),
+                         roll * RAD2DEG, pitch * RAD2DEG, yaw * RAD2DEG);
+#endif
+
+      // Publish odometry message
+      publishOdom(mOdom2BaseTransf, deltaOdom, mFrameTimestamp);
+      mPosTrackingReady = true;
+    }
+  }
+  else if (mFloorAlignment)
+  {
+    RCLCPP_DEBUG_THROTTLE(get_logger(), steady_clock, 5.0,
+                          "Odometry will be published as soon as the floor as been detected for the first time");
+  }
+}
+
+
+void ZedCamera::publishOdom(tf2::Transform& odom2baseTransf, sl::Pose& slPose, rclcpp::Time t)
+{
+  //adapted from https://github.com/stereolabs/zed-ros2-wrapper/blob/de1dfc4cde9ada3173074a05e14631d4d56e442c/zed_components/src/zed_camera/src/zed_camera_component.cpp
+  
+  odomMsgPtr odomMsg = std::make_unique<nav_msgs::msg::Odometry>();
+
+  odomMsg->header.stamp = t;
+  odomMsg->header.frame_id = mOdomFrameId;  // frame
+  odomMsg->child_frame_id = mBaseFrameId;   // camera_frame
+
+  // Add all value in odometry message
+  odomMsg->pose.pose.position.x = odom2baseTransf.getOrigin().x();
+  odomMsg->pose.pose.position.y = odom2baseTransf.getOrigin().y();
+  odomMsg->pose.pose.position.z = odom2baseTransf.getOrigin().z();
+  odomMsg->pose.pose.orientation.x = odom2baseTransf.getRotation().x();
+  odomMsg->pose.pose.orientation.y = odom2baseTransf.getRotation().y();
+  odomMsg->pose.pose.orientation.z = odom2baseTransf.getRotation().z();
+  odomMsg->pose.pose.orientation.w = odom2baseTransf.getRotation().w();
+
+  // Odometry pose covariance
+  for (size_t i = 0; i < odomMsg->pose.covariance.size(); i++)
+  {
+    odomMsg->pose.covariance[i] = static_cast<double>(slPose.pose_covariance[i]);
+
+    if (mTwoDMode)
+    {
+      if (i == 14 || i == 21 || i == 28)
+      {
+        odomMsg->pose.covariance[i] = 1e-9;  // Very low covariance if 2D mode
+      }
+      else if ((i >= 2 && i <= 4) || (i >= 8 && i <= 10) || (i >= 12 && i <= 13) || (i >= 15 && i <= 16) ||
+               (i >= 18 && i <= 20) || (i == 22) || (i >= 24 && i <= 27))
+      {
+        odomMsg->pose.covariance[i] = 0.0;
+      }
+    }
+  }
+
+  // Publish odometry message
+  publish_odometry->publish(std::move(odomMsg));
+}
 
 void ZedPublisher::fillCamInfo(sl::Camera& zed, sl::Resolution& resolution, std::shared_ptr<sensor_msgs::msg::CameraInfo> leftCamInfoMsg,
                             std::shared_ptr<sensor_msgs::msg::CameraInfo> rightCamInfoMsg, std::string leftFrameId,
